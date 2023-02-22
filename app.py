307e75fb1652dea464b3f041bf3d9c8ca4cb3050
Theo Baker <theocgbaker@gmail.com>
2023-02-22 15:57:16 +1300

    made complex values have straight line values

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ x = list(range(-1*num_values, num_values+1))
 x.remove(0)
 
 y = [i**(1/i) for i in x]
+y = [np.sqrt(i.real**2+i.imag**2) if type(i) == complex else i for i in y ]
 
 plt.plot(x, y)
 plt.show()
